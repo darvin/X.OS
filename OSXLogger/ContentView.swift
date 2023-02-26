b84607f35419e946f9612e4b7976f0f409a381c7
stc <standardtemplateconstruct2022@gmail.com>
2023-02-26 14:16:18 +0200

    initial sample integration

--- OSXLogger/ContentView.swift
+++ OSXLogger/ContentView.swift
@@ -7,15 +7,63 @@
 
 import SwiftUI
 
+
+import SwiftUI
+import ScreenCaptureKit
+import OSLog
+import Combine
+
 struct ContentView: View {
+    
+    @State var userStopped = false
+    @State var disableInput = false
+    @State var isUnauthorized = false
+    
+    @StateObject var screenRecorder = ScreenRecorder()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            screenRecorder.capturePreview
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .aspectRatio(screenRecorder.contentSize, contentMode: .fit)
+                .padding(8)
+                .overlay {
+                    if userStopped {
+                        Image(systemName: "nosign")
+                            .font(.system(size: 250, weight: .bold))
+                            .foregroundColor(Color(white: 0.3, opacity: 1.0))
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(Color(white: 0.0, opacity: 0.5))
+                    }
+                }
+        .overlay {
+            if isUnauthorized {
+                VStack() {
+                    Spacer()
+                    VStack {
+                        Text("No screen recording permission.")
+                            .font(.largeTitle)
+                            .padding(.top)
+                        Text("Open System Settings and go to Privacy & Security > Screen Recording to grant permission.")
+                            .font(.title2)
+                            .padding(.bottom)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .background(.red)
+                    
+                }
+            }
         }
-        .padding()
+        .navigationTitle("Screen Capture Sample")
+        .onAppear {
+            Task {
+                if await screenRecorder.canRecord {
+                    await screenRecorder.start()
+                } else {
+                    isUnauthorized = true
+                    disableInput = true
+                }
+            }
+        }
     }
 }
 
