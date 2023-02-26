44e64f72a5268cec94bd165c5fd1f35955de8eec
stc <standardtemplateconstruct2022@gmail.com>
2023-02-26 14:08:08 +0200

    Initial Commit

---
+++ OSXLogger/OSXLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OSXLoggerApp.swift
+//  OSXLogger
+//
+//  Created by standard on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct OSXLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
